b92bc86083215dd35483dfa6e08d853b769e854e
Aurelie05 <roxaneakredjoro@gmail.com>
2025-04-04 09:54:31 +0000

    quatre

--- resources/views/pdf/recu.blade.php
+++ resources/views/pdf/recu.blade.php
@@ -5,16 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reçu d'Inscription</title>
     <style>
-        body { font-family: Georgia, serif; margin: 20px; }
+        body { font-family: Georgia, serif; margin: 20px;  padding-top: 90px;}
         .header { text-align: right; font-size: 14px; margin-bottom: 10px; }
         .content { margin-top: 20px; }
         .logo img { width: 120px; }
         .title { text-align: center; font-weight: bold; font-size: 18px; }
         .bold { font-weight: bold; }
+        .title span {
+            color: red; /* ✅ Numéro rouge */
+        }
         .logo { margin-bottom: 10px; }
         .logo img { width: 100px; }
         .qr-code { margin: 20px auto; }
-        .qr-code img { width: 150px; }
+        .qr-code img { width: 150px;}
         .footer { margin-top: 20px; font-size: 12px; }
     </style>
 </head>
@@ -24,12 +27,13 @@
     </div>
     
     <div class="logo">
-        <img src="{{ asset('Asset/Icon.png') }}" alt="Logo INP-HB">
+        <img src="{{ asset('assests/Icon.png') }}" alt="Logo INP-HB">
     </div>
     
     <p style="font-size: 16px; font-weight: bold;">INP-HB</p>
-    <p style="font-size: 14px;">FABLAB ACADEMY 2025</p>
-    <p class="title">FACTURE D’INSCRIPTION N° {{ $renseignement->numero_enregistrement }}</p>
+    <p style="font-size: 14px; font-weight: bold;">FABLAB ACADEMY 2025</p>
+    <p class="title">FACTURE D’INSCRIPTION N° <span>{{ $renseignement->numero_enregistrement }}</span></p>
+
 
     <div class="content">
         <p><span class="bold">NOM DU PARENT :</span> {{ $renseignement->nom }} {{ $renseignement->prenoms }}</p>
@@ -40,7 +44,7 @@
     </div>
     <!-- QR Code -->
     <div class="qr-code">
-        <img src="" alt="QR Code">
+    <img src="{{ asset('assests/qrcode.png') }}" alt="QR Code">
     </div>
 
     <div class="footer">
