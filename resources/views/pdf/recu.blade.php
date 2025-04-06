f8ceba3a0d11a7e76d621a775f9ec87c48609128
Aurelie05 <roxaneakredjoro@gmail.com>
2025-04-06 20:52:07 +0000

    quatorze

--- resources/views/pdf/recu.blade.php
+++ resources/views/pdf/recu.blade.php
@@ -27,7 +27,7 @@
     </div>
     
     <div class="logo">
-        <img src="{{ asset('assests/Icon.png') }}" alt="Logo INP-HB">
+        <img src="../Assets/Icon.png" alt="Logo INP-HB">
     </div>
     
     <p style="font-size: 16px; font-weight: bold;">INP-HB</p>
